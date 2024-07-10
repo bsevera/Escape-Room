98b28df056d3e1c6135351d213ab3a0ca685e11d
Brian Severa <bsevera@new.rr.com>
2024-07-10 18:08:12 -0500

    configured padlocks on escape door

---
+++ Assets/Scripts/PadlockController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadlockController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject PadLock1;
+    [SerializeField] 
+    private GameObject PadLock2;
+    [SerializeField]
+    private GameObject Key1;
+    [SerializeField] 
+    private GameObject Key2;
+
+    private int _KeysInserted = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void KeyInserted()
+    {
+        _KeysInserted++;
+        
+        if (_KeysInserted == 2)
+        {
+            DisablePadlocksAndKeys();
+        }
+    }
+
+    public void KeyRemoved()
+    {
+        _KeysInserted--;
+    }
+
+    private void DisablePadlocksAndKeys()
+    {
+        PadLock1.SetActive(false);
+        PadLock2.SetActive(false);
+
+        Key1.SetActive(false);
+        Key2.SetActive(false);
+    }
+}
