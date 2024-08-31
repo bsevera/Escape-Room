5e95b61ba16373fc50549c343147a149213bec32
Brian Severa <bsevera@new.rr.com>
2024-08-30 20:30:28 -0500

    Letter puzzle logic completed

---
+++ Assets/Scripts/DrainGrateLock.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrainGrateLock : MonoBehaviour
+{
+    private string key = "ZURN";
+
+    [SerializeField]
+    private GameObject[] _LetterCanvases = new GameObject[4];
+    [SerializeField]
+    private AudioSource _AudioSource;
+
+    private int _KeyCountDown = 0;
+
+    public void Start()
+    {
+        for (int i = 0; i < _LetterCanvases.Length; i++)
+        {
+            _KeyCountDown += (i + 1);
+        }
+        Debug.Log("KeyCountdown (Start) = " + _KeyCountDown);
+    }
+
+    public void EnableLetter(string letter)
+    {
+        int index = key.IndexOf(letter);
+        if (index > -1)
+        {
+            _LetterCanvases[index].SetActive(true);
+            CheckKeyCountdown(index + 1);
+        }
+    }
+
+    private void CheckKeyCountdown(int value)
+    {
+        _KeyCountDown -= value;
+
+        Debug.Log("KeyCountdown = " + _KeyCountDown);
+
+        if (_KeyCountDown == 0)
+        {
+            //Play grate opening audio and animate grate opening
+            _AudioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ZURNLetter.cs
+++ Assets/Scripts/ZURNLetter.cs
@@ -8,8 +8,34 @@ public class ZURNLetter : MonoBehaviour
     [SerializeField]
     private TMP_Text _letter;
 
+    [SerializeField] private GameObject _DrainGrate = null;
+    private DrainGrateLock _DrainGrateLock;
+
+    private AudioSource _AudioSource;
+    private bool _LetterFound = false;
+
+    public void Start()
+    {
+        _DrainGrateLock = _DrainGrate.GetComponent<DrainGrateLock>();
+        _AudioSource = _DrainGrate.GetComponent<AudioSource>();
+    }
+
+
     public void IlluminateLetter()
     {
-        _letter.alpha = 42;
+        Color tmpColor = _letter.color;
+        _letter.alpha = 1;
+
+        if (!_LetterFound)
+        {
+            _LetterFound = true;
+            _DrainGrateLock.EnableLetter(_letter.text);
+            _AudioSource.time = 1;
+            _AudioSource.Play();
+        }
+    }
+    public void HideLetter()
+    {
+        _letter.alpha = 0;
     }
 }
