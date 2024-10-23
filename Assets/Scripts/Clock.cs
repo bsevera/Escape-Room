4d60e4491b883e3c6d0403e79d12b3fab3b0096d
Brian Severa <bsevera@new.rr.com>
2024-10-22 19:46:26 -0500

    added reward audio when key appears

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -10,17 +10,15 @@ public class Clock : MonoBehaviour
     [SerializeField]
     private GameObject _BronzeKey;
 
+    private AudioSource _audioSource = null;
+    private bool _audioPlayed = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audioSource = GetComponent<AudioSource>();               
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void DoGazeRegistration()
     {
@@ -36,6 +34,12 @@ public class Clock : MonoBehaviour
 
         if (other.tag == "BTTF")
         {
+            if (!_audioPlayed)
+            {
+                _audioSource.Play();
+                _audioPlayed = true;
+            }
+
             _BronzeKey.SetActive(true);
             _BronzeKey.transform.parent = null;
         }
