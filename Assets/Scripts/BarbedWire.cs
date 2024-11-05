2a04bccfef1d1d84369a32711ae7efa3f098f4a1
Brian Severa <bsevera@new.rr.com>
2024-11-04 20:15:24 -0600

    created interactions between barbed wire and pliers

---
+++ Assets/Scripts/BarbedWire.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarbedWire : MonoBehaviour
+{
+    private AudioSource _audioSource = null;
+    public void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ClipWire()
+    {
+        //_audioSource.Play();        
+        this.gameObject.SetActive(false);
+    }
+    
+}
